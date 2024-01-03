f0123024ca66d263319ed301ef2524950f0dd939
RimidalU <stankevichuuladimiru@gmail.com>
2024-01-04 00:17:10 +0300

    feat: implement AdditionTwoNumbers class

---
+++ src/AdditionTwoNumbers.java
@@ -0,0 +1,7 @@
+public class AdditionTwoNumbers {
+    public static void main(String[] args) {
+        double num1 = 20.2, num2 = 2.1, amount;
+        amount = num1 + num2;
+        System.out.println("The amount of " + num1 + " and " + num2 + " is equal " + amount);
+    }
+}
